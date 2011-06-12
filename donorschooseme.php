e96499b4d849b4494340737ecec8deacd37130d1
John Mertens <john.mertens@gmail.com>
2011-06-12 00:51:25 -0700

    Actually display something to the screen.

--- donorschooseme.php
+++ donorschooseme.php
@@ -25,14 +25,27 @@ License: GPL2
 */
 
 function get_donors_choose_projects() {
+    $ret = '';
     $data = file_get_contents('http://api.donorschoose.org/common/json_feed.html?APIKey=DONORSCHOOSE&centerLat=40.776104&centerLng=-73.920822');
     if($data) {
         $json_data = json_decode($data);
         $projects = array_slice($json_data->proposals, 0, 3);
-        
-        echo print_r($projects, true);
+        foreach($projects as $proj) {
+            $ret .= render_dc_project_html($proj);
+        }
+        echo $ret;
     } else {
         echo 'Could not connect to page.';
     }
+}
+
+function render_dc_project_html($project) {
+    $html = '<div class="dc-project">'.
+            '<img src="'.$project->imageURL.'" />'.
+            '<span class="dc-project-title"><a class="dc-project-link" href="'.$project->proposalURL.'">'.$project->title.'</a></span>'.
+            '<span class="dc-project-location">'.$project->zone->name.', '.$project->state.'</span>'.
+            '<span class="dc-project-funding-appeal"><a class="dc-project-funding-link" href="'.$project->fundURL.'">'.$project->costToComplete.'</a></span>'.
+            '</div>';
+    return $html;
 }
 ?>
