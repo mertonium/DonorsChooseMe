da7f7e9e20f2ab63a02c4b59c2bdeb96d885d490
John Mertens <john.mertens@gmail.com>
2011-06-12 01:24:08 -0700

    Add geolocation of users IP address.

--- donorschooseme.php
+++ donorschooseme.php
@@ -24,28 +24,40 @@ License: GPL2
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-function get_donors_choose_projects() {
-    $ret = '';
-    $data = file_get_contents('http://api.donorschoose.org/common/json_feed.html?APIKey=DONORSCHOOSE&centerLat=40.776104&centerLng=-73.920822');
-    if($data) {
-        $json_data = json_decode($data);
-        $projects = array_slice($json_data->proposals, 0, 3);
-        foreach($projects as $proj) {
-            $ret .= render_dc_project_html($proj);
+class DonorsChoosePlugin {
+    
+    public static function get_projects() {
+        // SF   174.253.235.90
+        // GOOG 74.125.224.82 
+        $ip = '174.253.235.90'; //gethostbyname($_SERVER['SERVER_NAME']);
+    echo '<h1>' . $ip . '</h1>';
+        $ip_data = file_get_contents('http://api.ipinfodb.com/v3/ip-city/?key=cf74017054f12362f1ddecbf26ca61d8ab8acc5253573cdfe1a7f7255a4fdcc2&ip='.$ip);
+        if($ip_data) {
+            $ip_info = explode(';', $ip_data);
+            print_r($ip_info);
         }
-        echo $ret;
-    } else {
-        echo 'Could not connect to page.';
+        $ret = '';
+        $data = file_get_contents('http://api.donorschoose.org/common/json_feed.html?APIKey=DONORSCHOOSE&centerLat='.$ip_info[8].'&centerLng='.$ip_info[9]);
+        if($data) {
+            $json_data = json_decode($data);
+            $projects = array_slice($json_data->proposals, 0, 3);
+            foreach($projects as $proj) {
+                $ret .= self::render_project_html($proj);
+            }
+            echo $ret;
+        } else {
+            echo 'Could not connect to page.';
+        }
     }
-}
 
-function render_dc_project_html($project) {
-    $html = '<div class="dc-project">'.
-            '<img src="'.$project->imageURL.'" />'.
-            '<span class="dc-project-title"><a class="dc-project-link" href="'.$project->proposalURL.'">'.$project->title.'</a></span>'.
-            '<span class="dc-project-location">'.$project->zone->name.', '.$project->state.'</span>'.
-            '<span class="dc-project-funding-appeal"><a class="dc-project-funding-link" href="'.$project->fundURL.'">'.$project->costToComplete.'</a></span>'.
-            '</div>';
-    return $html;
+    private function render_project_html($project) {
+        $html = '<div class="dc-project">'.
+                '<img src="'.$project->imageURL.'" />'.
+                '<span class="dc-project-title"><a class="dc-project-link" href="'.$project->proposalURL.'">'.$project->title.'</a></span>'.
+                '<span class="dc-project-location">'.$project->zone->name.', '.$project->state.'</span>'.
+                '<span class="dc-project-funding-appeal"><a class="dc-project-funding-link" href="'.$project->fundURL.'">'.$project->costToComplete.'</a></span>'.
+                '</div>';
+        return $html;
+    }
 }
 ?>
